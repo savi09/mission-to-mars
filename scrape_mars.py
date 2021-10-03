172ddcfa81708a3aac03f54f1a7cb61d8c21c7e3
savi09 <savi@ucdavis.edu>
2021-10-03 03:03:47 -0700

    Having issues-trying to fix issues with image url dictionary

--- scrape_mars.py
+++ scrape_mars.py
@@ -161,6 +161,11 @@ def scrape():
         "html_table": html_table
     }
 
+    data = {
+        "mars_data": mars_data,
+        "hemisphere_image_urls": hemisphere_image_urls
+    }
 
-    return mars_data, hemisphere_image_urls
+    return data
+    
 
