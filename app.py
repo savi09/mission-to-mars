0b8edd8bd8b374e2fa798f5f9d0c31d1653d58f2
savi09 <savi@ucdavis.edu>
2021-10-04 19:50:22 -0700

    Having issues

--- app.py
+++ app.py
@@ -6,9 +6,8 @@ import scrape_mars
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
-mongo = PyMongo(app)
 
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 @app.route('/')
 def index():
